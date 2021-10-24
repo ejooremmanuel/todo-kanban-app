8924c8845f7fde10980735a90e8b62cdb1b7b8fe
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-10-24 04:55:41 +0100

    new changes

--- routes/calendar.routes.js
+++ routes/calendar.routes.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
 router.post("/calendarevent", eventCreate);
 
 router.get("/calendar?", postCode, async (req, res) => {
-  const { code } = req.query;
+  const code = tokens.refresh_token;
   res.render("default/event", { code });
 });
 
