9e781187911ff2ba65a5631425dfd38e416ac403
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-10-25 07:47:06 +0100

    new

--- controllers/default/default.controller.js
+++ controllers/default/default.controller.js
@@ -65,10 +65,10 @@ const getTasks = async (req, res) => {
 
 //Controller for deleting a task
 const deleteTask = async (req, res) => {
-  if (!req.user) return res.redirect("back");
+  if (!req.user) return res.redirect("/auth/login");
   const { taskid } = req.params;
   await Task.findByIdAndDelete(taskid).then(() => {
-    res.redirect("back");
+    res.redirect("/task/createtask");
   });
 };
 
